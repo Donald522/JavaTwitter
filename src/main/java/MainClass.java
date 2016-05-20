fde7a8b26c0020a68e9a5b71f52b4ff7719bba73
Donald522 <Panther5G>
2016-05-20 15:43:43 +0300

    last version

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -12,26 +12,28 @@ public class MainClass {
 
         tweets.addAll(tweetsCollection);
         System.out.println("number of tweets = " + tweets.size());
-
+/*
         System.out.println("=======Without sort========");
         for(Tweet t : tweets) {
             System.out.println(t);
             System.out.println("--------------------");
         }
-
+*/
+/*
         System.out.println("=======Sort by Retweets========");
         tweets.sort(new SortedByRetweets());
         for(Tweet t : tweets) {
             System.out.println(t);
             System.out.println("--------------------");
         }
+*/
         System.out.println("=======Top Rated=======");
         System.out.println(tweets.getTopRated());
-
+/*
         System.out.println("========Group by lang=======");
         System.out.println(tweets.groupByLang());
-
-        System.out.println(tweets.getTagCloud("en"));
+*/
+//      System.out.println(tweets.getTagCloud("en"));
 
     }
 }
